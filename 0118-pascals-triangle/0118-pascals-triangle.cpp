e3908b03e41c8dbcf1a640a5cc20b9af9cc6e018
DurvishKumar <durvishkumar1271@gmail.com>
2026-06-29 01:06:19 +0530

    Time: 0 ms (100%), Space: 9.5 MB (90.01%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int>generaterow(int rows){
+        int ans=1;
+        vector<int>ansRow;
+        ansRow.push_back(1);
+        for(int i=1; i<rows; i++){ 
+            ans=ans*(rows-i);
+            ans=ans/i;
+            ansRow.push_back(ans);
+            
+        }
+        return ansRow;
+    }
+    vector<vector<int>> generate(int numRows) {
+
+        vector<vector<int>>ans;
+        for(int i=1; i<=numRows; i++){
+            ans.push_back(generaterow(i));
+        }
+        return ans;
+    }
+};
