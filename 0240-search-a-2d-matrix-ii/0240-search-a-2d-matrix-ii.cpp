af2cba1e0613587ec64d9ff70132e9a84a570a98
DurvishKumar <durvishkumar1271@gmail.com>
2026-07-06 18:59:12 +0530

    Time: 59 ms (30.54%), Space: 18.8 MB (38.31%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    bool isPresent(vector<int> &matrix, int n, int target){
-        int low = 0;
-        int high = n-1;
-        while(low <= high){
-            int mid = low + (high-low)/2;
-            if(matrix[mid] == target) return true;
-            else if(matrix[mid] < target) low = mid+1;
-            else high = mid-1;
-        }
-        return false;
-    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        for(int row = 0; row < m; row++){
-            if(isPresent(matrix[row], n, target) == true) return true;
-        } 
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int row = 0;
+        int col = m-1;
+        while(row < n && col >=0){
+            if(matrix[row][col] == target) return true;
+            else if(matrix[row][col] < target) row++;
+            else col--;
+        }
         return false;
     }
 };
