87f27f36bef7c2ef0ca4f925251c476f450c01b9
DurvishKumar <durvishkumar1271@gmail.com>
2026-07-25 22:50:12 +0530

    Time: 17 ms (18.46%), Space: 22.6 MB (23.07%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int>st;
+        int n = heights.size();
+        int maxArea = 0;
+        vector<int>pse(n,-1);
+        vector<int>nse(n,n);
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && heights[st.top()] >= heights[i]){
+                nse[st.top()] = i;
+                st.pop();
+            }
+            if(!st.empty()) pse[i] = st.top();
+            st.push(i);
+        }
+        for(int i = 0; i < n; i++){
+            maxArea = max(maxArea,(heights[i] * (nse[i] - pse[i] - 1)));
+        }
+        return maxArea;
+    }
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int maxRectangle = 0;
+        vector<vector<int>> preSum(n, vector<int>(m));
+        for(int j = 0; j < m; j++){
+            int sum = 0;
+            for(int i = 0; i < n; i++){
+                if(matrix[i][j] == '1') sum++;
+                else sum = 0;
+                preSum[i][j] = sum;
+            }
+        }
+        for(int i = 0; i < n; i++){
+            maxRectangle = max(maxRectangle , largestRectangleArea(preSum[i]));
+        }
+        return maxRectangle;
+    }
+};
